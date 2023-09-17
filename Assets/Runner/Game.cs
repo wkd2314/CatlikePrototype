90d9eaf06849c4a7eaa279f8d765273e67410c6d
장원익 <somesome2314@gmail.com>
2023-09-18 04:15:45 +0900

    .

---
+++ Assets/Runner/Game.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField]
+    Runner runner;
+
+    [SerializeField]
+    TrackingCamera trackingCamera;
+
+    [SerializeField]
+    TextMeshPro displayText;
+
+    [SerializeField, Min(0.001f)]
+    float maxDeltaTime = 1f / 120f;
+
+    bool isPlaying;
+
+    void StartNewGame()
+    {
+        trackingCamera.StartNewGame();
+        runner.StartNewGame();
+        isPlaying = true;
+    }
+
+    void Update()
+    {
+        if (isPlaying)
+        {
+            UpdateGame();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartNewGame();
+        }
+    }
+
+    void UpdateGame()
+    {
+        float accumulateDeltaTime = Time.deltaTime;
+        while (accumulateDeltaTime > maxDeltaTime && isPlaying)
+        {
+            isPlaying = runner.Run(maxDeltaTime);
+            accumulateDeltaTime -= maxDeltaTime;
+        }
+        isPlaying = isPlaying && runner.Run(accumulateDeltaTime);
+        runner.UpdateVisualization();
+        trackingCamera.Track(runner.Position);
+        displayText.SetText("{0}", Mathf.Floor(runner.Position.x));
+    }
+}
---
+++ Assets/Runner/TrackingCamera.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingCamera : MonoBehaviour
+{
+    Vector3 offset, position;
+
+    void Awake()
+    {
+        offset = transform.localPosition;
+    }
+
+    public void StartNewGame()
+    {
+        Track(Vector3.zero);
+    }
+
+    public void Track(Vector3 focusPoint)
+    {
+        position = focusPoint + offset;
+        transform.localPosition = position;
+    }
+}
